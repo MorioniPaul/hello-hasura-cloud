baf9566f85c715dcc63387de184443fc51d74337
Paul Morioni <paul.morioni@saile.ai>
2024-01-11 23:02:39 -0600

    removed sql that couldnt run

--- hasura/configuration/migrations/default/1705008220907_create_table_public_products/up.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE "public"."products" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "created_at" timestamptz NOT NULL DEFAULT now(), PRIMARY KEY ("id") );
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
